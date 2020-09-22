be9642f74663117e7f4270a5833829a5b4539c45
Mohammad sobuj <mohammad@sobuj.gov>
2020-09-23 03:30:48 +0600

    add class in python

---
+++ class.py
@@ -0,0 +1,90 @@
+'''
+print()
+print('class is most important in python')
+print("I am successfull in python")
+print(
+    """
+    this procedure is at a time print in multiline 
+    First line print
+    Second line print
+    Third line print 
+    Four line print
+    Five line print
+    Finally do the work successfully
+    So I am Happy
+
+    
+    """
+)
+
+
+print('How does it possible a good programmar in Wevdepoler')
+print("Realy i  have good programmar in this time ")
+print()
+
+'''
+
+# Class discussion in this subject  
+class Point:
+    class_name='Point'
+    def move(self,c_name):
+        print("hello move function")
+        print(c_name)
+        print(f"class name is: {c_name}")
+
+    def draw(self,name,age):
+        self.age=age
+        print(self.age)
+
+    
+        print("hello draw function")
+        print("Hi you "+name)
+    def information(name,age,address,gender):
+         print('Hi')
+         print(age)
+         print(address)
+         print(gender)   
+
+    x=101;  
+    def  __init__(self,a,b):
+        self.a=a
+        self.b=b   
+    def myfun(abc):
+        print(f"hello constractor {abc.a}")
+
+class person:
+
+    pass
+
+
+pr=person()
+pr.y=35
+print(pr.y)
+
+p=Point(122,44)
+print(p.a)
+p.move('sobuj')
+p.myfun()
+p.time=4
+p.time=434
+print(p.time)
+#del p.time
+print(p.time)
+#obj=Point()
+#obj.move(obj.class_name)
+#obj.draw('sobuj',43)
+#print(obj.class_name)
+#p1=Point()
+#print(p1.x)
+#p1.name="sobuj islam"
+#print(p1.x)
+#print(p1.name)
+#p1.information(34,'dhaka,malibagh,1217','male')
+#p2=Point()
+ 
+
+
+
+
+
+
